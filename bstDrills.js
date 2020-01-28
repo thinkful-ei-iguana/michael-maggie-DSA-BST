5c5a69b59d0b074411a05954c511f8de0638245f
Michael Weedman <michael.weedman@gmail.com>
2020-01-28 10:41:06 -0700

    5 completed

--- bstDrills.js
+++ bstDrills.js
@@ -14,7 +14,7 @@ https://gist.github.com/mweedman/a8f619fef6233b709c99eb1667c9d4fe
 
 //3 - Create the BST class
 
-function main() {
+function numberTree() {
   let firstTree = new BST();
   firstTree.insert(3, 3);
   firstTree.insert(1, 1);
@@ -25,15 +25,62 @@ function main() {
   firstTree.insert(5, 5);
   firstTree.insert(7, 7);
 
-
   return firstTree;
 }
 
-console.log(main());
+function letterTree() {
+  let lTree = new BST();
+
+  lTree.insert('E', 'E');
+  lTree.insert('A', 'A');
+  lTree.insert('S', 'S');
+  lTree.insert('Y', 'Y');
+  lTree.insert('Q', 'Q');
+  lTree.insert('U', 'U');
+  lTree.insert('E', 'E');
+  lTree.insert('S', 'S');
+  lTree.insert('T', 'T');
+  lTree.insert('I', 'I');
+  lTree.insert('O', 'O');
+  lTree.insert('N', 'N');
+
+  lTree.remove('E');
+  // console.log('@Y: ', lTree.find('Y'));
+  return lTree;
+}
+ 
+// console.log(numberTree());
+// letterTree();
+// console.log();
 
 //4 - What does this program do?
 
+//  This function sums the values of all of the nodes in the tree. //
+
 //5 - Height of a BST
+let heightCount = 0;
+function height(tree) {
+  
+  if (!tree) {
+    return;
+  }
+  else if (!tree.left && !tree.right) {
+    heightCount++;
+    return heightCount;
+  }
+  else if (tree.right) {
+    heightCount++;
+    height(tree.right);
+  }
+  else if (tree.left) {
+    heightCount++;
+    height(tree.left);
+  }
+
+  return 'The height of the tree is: ' + heightCount;
+}
+
+console.log(height(numberTree()));
 
 //6 - Is it a BST?
 
